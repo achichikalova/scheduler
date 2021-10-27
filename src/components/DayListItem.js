18417c0ca560eeff20af7119563c3d739d81b231
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-10-27 14:42:36 -0400

    Refactore code

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,12 +7,25 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   });
+  
+  //doesn't work
+  const formatSpots = (spots) => {
+    if (spots === 1) {
+      return `1 spot remaining`
+    } else if (spots === 0) {
+      return `no spots remaining`
+    } else {
+      return `${spots} spots remaining`
+    }
+  }
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">
-        {props.spots === 1 ? `${props.spots} spot` : props.spots === 0 ? 'no spots' : `${props.spots} spots`} remaining</h3>
+        {formatSpots(props.spots)}
+        {/* {props.spots === 1 ? `${props.spots} spot` : props.spots === 0 ? 'no spots' : `${props.spots} spots`} remaining */}
+      </h3>        
     </li>
   );
 }
