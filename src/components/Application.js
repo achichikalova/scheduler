6505d262b55abee10a490a96d9a8ff917fc0ffa0
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-11-01 20:03:53 -0400

    Create useApplicationData hook, refactor the Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,86 +1,37 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import React from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 
 export default function Application(props) {
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
 
-  const setDay = day => setState(prev => ({ ...prev, day }));
-
-   // Take data from the server to set up the initial state
-  useEffect(() => {
-    Promise.all([
-    axios.get('/api/days'),
-    axios.get('/api/appointments'),
-    axios.get('/api/interviewers')
-    ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    })
-  }, [])
-
-  // Add new interview in the appointments state and make the PUT request from server
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-  
-    return axios
-      .put(`/api/appointments/${id}`, { interview })
-      .then(() => {
-        setState({...state, appointments});
-      })
-  };
-
-  //Deleting appointment from the appointments state and make DELETE request
-  const cancelInterview = (id) => {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios
-      .delete(`/api/appointments/${id}`)
-      .then(() =>  setState({...state, appointments}))
-  };
-
   //Create the Appointment Components
-  const schedule = dailyAppointments.map((appointment) => {
+  const schedule = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
     return (
       <Appointment
-      key={appointment.id}
-      id={appointment.id}
-      time={appointment.time}
-      interview={interview}
-      interviewers={interviewers}
-      bookInterview={bookInterview}
-      cancelInterview={cancelInterview}
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+        interviewers={interviewers}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
-      );
+    );
   });
   
   //Render our component with needed props within
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function useApplicationData() {
+  //
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  // Set day
+  const setDay = day => setState(prev => ({ ...prev, day }));
+
+  // Take data from the server to set up the initial state
+  useEffect(() => {
+    Promise.all([
+    axios.get('/api/days'),
+    axios.get('/api/appointments'),
+    axios.get('/api/interviewers')
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    })
+  }, [])
+
+  // Add new interview in the appointments state and make the PUT request from server
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+  
+    return axios
+      .put(`/api/appointments/${id}`, { interview })
+      .then(() => {
+        setState({...state, appointments});
+      })
+  };
+
+  //Deleting appointment from the appointments state and make DELETE request
+  const cancelInterview = (id) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios
+      .delete(`/api/appointments/${id}`)
+      .then(() =>  setState({...state, appointments}))
+  };
+  
+  
+  return { state, setDay, bookInterview, cancelInterview };
+  
+}
