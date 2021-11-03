167ec99e63b167e27289a7b46841077b49d21164
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-11-03 15:12:47 -0400

    Implement first Cypress test navigation.spec.js

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,24 @@
+// "should book an interview"
+// "should edit an interview"
+// "should cancel an interview"
+
+// Steps for Booking:
+// Visits the root of our web server
+// Clicks on the "Add" button in the second appointment
+// Enters their name
+// Chooses an interviewer
+// Clicks the save button
+// Sees the booked appointment
+
+// Steps for Editing:
+// Visits the root of our web server
+// Clicks the edit button for the existing appointment
+// Changes the name and interviewer
+// Clicks the save button
+// Sees the edit to the appointment
+
+// //Steps for Canceling:
+// Visits the root of our web server
+// Clicks the delete button for the existing appointment
+// Clicks the confirm button
+// Sees that the appointment slot is empty
